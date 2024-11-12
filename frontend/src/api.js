3d741f720ec7438b35e380f4d492091368abd2ab
Ankush Kumar Kushwaha <ankush@radiantinfonet.com>
2024-11-12 16:57:57 +0530

    api url updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/notes';
+const API_URL = 'https://notesapp-52i8.onrender.com';
 
 export const fetchNotes = async () => axios.get(API_URL);
 export const addNote = async (content) => axios.post(`${API_URL}/add`, { content });
